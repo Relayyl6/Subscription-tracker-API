2bbac0019963ec24a3aea1a76b0aeb05489182ee
Leonard <Oseghaleleonard39@gmail.com>
2025-10-31 16:12:18 +0100

    Successfully set up arcjet protection

--- app.js
+++ app.js
@@ -9,18 +9,22 @@ import connectToDatabase from "./database/mongodb.js";
 
 import errorMiddleware from "./middleware/error.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleWare from "./middleware/arcjet.middleware.js";
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser());
+app.use(arcjetMiddleWare());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
+
 
 
 app.get('/', (req, res) => {
